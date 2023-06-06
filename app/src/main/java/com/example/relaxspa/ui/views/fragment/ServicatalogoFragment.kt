6aad97b12651b06ed9c53c531a9adc047e026ce5
Jack <jack.villafuerte.s@gmail.com>
2023-06-05 22:10:57 -0500

    commit de catalogo y contactos Fragment

---
+++ app/src/main/java/com/example/relaxspa/ui/views/fragment/ServicatalogoFragment.kt
@@ -0,0 +1,43 @@
+package com.example.relaxspa.ui.views.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.relaxspa.R
+import com.example.relaxspa.databinding.FragmentRegistroBinding
+import com.example.relaxspa.databinding.FragmentServicatalogoBinding
+
+
+class ServicatalogoFragment : Fragment() {
+
+    private var _binding: FragmentServicatalogoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentServicatalogoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnNuestroServi.setOnClickListener {
+            var action = ServicatalogoFragmentDirections.actionServicatalogoFragmentToServiciosFragment(null)//puede ser nulo
+            findNavController().navigate(action)
+        }
+
+        binding.btnCatalogo.setOnClickListener{
+
+            var action = ServicatalogoFragmentDirections.
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/relaxspa/ui/views/fragment/ServiciosFragment.kt
@@ -0,0 +1,64 @@
+package com.example.relaxspa.ui.views.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.relaxspa.Application
+import com.example.relaxspa.R
+import com.example.relaxspa.databinding.FragmentRegistroBinding
+import com.example.relaxspa.databinding.FragmentServiciosBinding
+import com.example.relaxspa.ui.viewmodel.UsuarioViewModel
+import com.example.relaxspa.ui.viewmodel.ViewModelFactory
+
+
+class ServiciosFragment : Fragment() {
+
+    private var _binding: FragmentServiciosBinding? = null
+
+    private val binding get() = _binding!!
+
+    //Importante agregar esto
+    private val pViewModel: UsuarioViewModel by activityViewModels {
+        val repository = (requireActivity().application as Application).repository
+        ViewModelFactory(repository)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentServiciosBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        
+        binding.btnServiSoli.setOnClickListener {
+            var action1 = ServiciosFragmentDirections.actionServiciosFragmentToSolicitadosFragment(null)//puede ser nulo
+            findNavController().navigate(action1)
+        }
+
+        binding.btnUsuarios.setOnClickListener {
+            var action1 = ServiciosFragmentDirections.actionServiciosFragmentToSolicitadosFragment(null)//puede ser nulo
+            findNavController().navigate(action1)
+        }
+
+        //soolo para ver la lista un observador
+        pViewModel.usuarios.observe(viewLifecycleOwner) {lista ->
+            println("INFORMACION DE LA LISTA ${lista.size}")
+            for (p in lista) {
+                println(p.toString())
+            }
+        }
+    }
+
+
+}
